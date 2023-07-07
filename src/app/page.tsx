226a66efde020559567117471e32b9c916cb8855
Rana <rantun@udel.edu>
2023-07-07 15:08:02 +0300

    Added frame of site, including header, sections, and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,87 @@
 import Image from 'next/image'
+import React from "react"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <body>
+      <header>
+        <nav>
+          <div className="mainheader">
+            <div>Some kinda icon</div>
+            <ul className="nav-links">
+              <li>
+                <a href="/#skills">Skills</a>
+              </li>
+              <li>
+                <a href="/#projects">Projects</a>
+              </li>
+              <li>
+                <a href="/#resume">Resume</a>
+              </li>
+              <li>
+                <a href="/#contact">Contact</a>
+              </li> 
+              <li>O</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <ul className="sidebar" aria-label="Media">
+          <li>v</li>
+          <li>
+            <a className="block hover:text-slate-200" href="https://github.com/nratun" target="_blank" rel="noreferrer">
+              {/* Consider adding span tag w/ text of each icon */}
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a className="block hover:text-slate-200" href="https://www.linkedin.com/in/rana-tuncer-304956281" target="_blank" rel="noreferrer">
+              {/* Consider adding span tag w/ text of each icon */}
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true">
+                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+              </svg>
+            </a>
+        </li>
+        <li>etc</li>
+        <li>
+          <a href="#top">top</a>
+        </li>
+      </ul>
+      <main className="mainEdges">
+        <section className="sectionEdges">
+          <h1 className="title">Rana Tuncer</h1>
+          <h2>Student at University of Delaware</h2>
+          <p>Majoring in Computer Science and minoring in Game Studies & Cognitive Science</p>
+        </section>
+        <section className="sectionEdges">
+          About Me
+          <div>
+            Hey! I’m Rana, a software developer and third year student at the University of Delaware. I’m currently pursuing a degree in Computer Science and minoring in Cognitive Science and Game Studies. In my free time, I like to experiment with new programming languages and software to create mini games and other personalized projects.
+          </div>
+        </section>
+        <section className="sectionEdges" id="skills">
+          Skills
+        </section>
+        <section className="sectionEdges" id="projects">
+          Projects
+        </section>
+        <section className="sectionEdges" id="resume">
+          Resume
+        </section>
+        <section className="sectionEdges" id="contact">
+          Contact
+        </section>
+      </main>
+    </body>
+  )
+}
+
+{/* <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Rana site
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -108,6 +183,4 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
-  )
-}
+    </main> */}
